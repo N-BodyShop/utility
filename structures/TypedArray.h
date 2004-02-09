39b8b7b9c15d773a01766a4f21387d32191638e7
Graeme Lufkin <gwl@u.washington.edu>
2004-02-09 18:03:54 +0000

    Added coercion from one type to another.

--- structures/TypedArray.h
+++ structures/TypedArray.h
@@ -13,12 +13,6 @@
 
 namespace TypeHandling {
 
-//This struct exists only to be a placeholder to allow template functions to be overloaded
-template <typename T>
-struct Type2Type {
-	typedef T OriginalType;
-};
-
 /** A TypedArray is the recommended way to store an array of values
  whose dimensionality and type are determined at runtime.  It provides
  methods for accessing the array of values in safe and unsafe ways,
@@ -217,6 +211,64 @@ inline double getScalarMax(const TypedArray& arr) {
 			return -HUGE_VAL;
 	}
 }
+
+template <typename T>
+struct Manipulator {
+	virtual T operator()(T value) {
+		return value;
+	}
+};
+
+template <typename T>
+T coerce(DataTypeCode code, void* data, int index, Type2Type<T>) {
+	switch(code) {
+		case int8: return static_cast<T>(reinterpret_cast<char *>(data)[index]);
+		case uint8: return static_cast<T>(reinterpret_cast<unsigned char *>(data)[index]);
+		case int16: return static_cast<T>(reinterpret_cast<short *>(data)[index]);
+		case uint16: return static_cast<T>(reinterpret_cast<unsigned short *>(data)[index]);
+		case int32: return static_cast<T>(reinterpret_cast<int *>(data)[index]);
+		case uint32: return static_cast<T>(reinterpret_cast<unsigned int *>(data)[index]);
+		case int64: return static_cast<T>(reinterpret_cast<int64_t *>(data)[index]);
+		case uint64: return static_cast<T>(reinterpret_cast<u_int64_t *>(data)[index]);
+		case float32: return static_cast<T>(reinterpret_cast<float *>(data)[index]);
+		case float64: return static_cast<T>(reinterpret_cast<double *>(data)[index]);
+		default: return static_cast<T>(reinterpret_cast<double *>(data)[index]);
+	}
+}
+
+template <typename T>
+Vector3D<T> coerce(DataTypeCode code, void* data, int index, Type2Type<Vector3D<T> >) {
+	switch(code) {
+		case int8: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<char> *>(data)[index]);
+		case uint8: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<unsigned char> *>(data)[index]);
+		case int16: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<short> *>(data)[index]);
+		case uint16: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<unsigned short> *>(data)[index]);
+		case int32: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<int> *>(data)[index]);
+		case uint32: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<unsigned int> *>(data)[index]);
+		case int64: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<int64_t> *>(data)[index]);
+		case uint64: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<u_int64_t> *>(data)[index]);
+		case float32: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<float> *>(data)[index]);
+		case float64: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<double> *>(data)[index]);
+		default: return static_cast<Vector3D<T> >(reinterpret_cast<Vector3D<double> *>(data)[index]);
+	}
+}
+
+template <typename T>
+class CoerciveExtractor {
+	const TypedArray& array;
+		
+public:
+	
+	CoerciveExtractor(const TypedArray& arr) : array(arr) {
+		//if(array.dimensions != Type2Dimensions<T>::dimensions)
+			//throw IncompatibleTypesException;
+	}
+
+	T operator[](int index) {
+		//if array.dimensions != Type2Dimensions<T>::dimensions BAD THINGS WILL HAPPEN!
+		return coerce(array.code, array.data, index, Type2Type<T>());
+	}
+};
 
 } //close namespace TypeHandling
 
