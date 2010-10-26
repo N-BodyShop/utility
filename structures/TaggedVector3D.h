82b492ca37b51eba318745ba4094cb6f1f1caae9
Pritish Jetley <pjetley2@illinois.edu>
2010-10-26 12:08:57 -0500

    tpindex needed for debugging multisteplb performance.

--- structures/TaggedVector3D.h
+++ structures/TaggedVector3D.h
@@ -11,10 +11,12 @@ class TaggedVector3D{
   unsigned int myNumParticles, numActiveParticles;
   unsigned int activeRung;
   int prevActiveRung;
+  int tpindex;
   int tag;
 
   TaggedVector3D(Vector3D<float> &v, LDObjHandle &i) : vec(v), handle(i){}
   TaggedVector3D(Vector3D<float> &v, LDObjHandle &i, unsigned int numAP, unsigned int myNP, unsigned int aR, unsigned int pAR) : vec(v), handle(i), numActiveParticles(numAP), myNumParticles(myNP), activeRung(aR), prevActiveRung(pAR){}
   TaggedVector3D() :numActiveParticles(0), myNumParticles(0), activeRung(0), prevActiveRung(0){}
+
 };
 #endif // TAGGED_VECTOR3D_H_
